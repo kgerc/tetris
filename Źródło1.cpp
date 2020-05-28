5029339132c722386bf23418208923f5c05d53e2
kgerc <50524005+kgerc@users.noreply.github.com>
2020-05-28 14:41:03 +0200

    Add files via upload
    końcowa wersja silnika

--- Źródło1.cpp
+++ Źródło1.cpp
@@ -6,87 +6,91 @@ using namespace std;
 int main()
 {
 	//Inicjalizacja planszy i pierwszego klocka
-	int x;
-	int x1=-2, y1=3,r_klocka=1,rotacja=0;
+	int x1=-2, y1=3,r_klocka=1,rotacja=0,x;
 	Plansza p1(20);
 	p1.Generator_Klockow(x1, y1, r_klocka, rotacja);
 	p1.Generator_Planszy();
 	//Giera tutaj
 	for(;;)
 	{
-		cout << endl<< "kierunek:";
-		cin >> x;
-		switch (x)
-		{
-		case 1:
-		{
-			p1.Czyszczenie_klockow(x1, y1, r_klocka, rotacja);
-			if (p1.czy_ruch_jest_mozliwy(x1 + 1, y1, r_klocka, rotacja))
-			{
-				p1.Generator_Klockow(x1 + 1, y1, r_klocka, rotacja);
-				x1++;
-			}
-			else
-			{
-				p1.Generator_Klockow(x1, y1, r_klocka, rotacja);
-				p1.Czy_usuwac_linie();
-				if (p1.KoniecGry() == true)
+		//cout << endl << "kierunek:";
+		//1-dó³
+		//2-prawo
+		//3-lewo
+		//4-rotacja
+			cin >> x;
+				switch (x)
 				{
-					cout << "GAME OVER";
+				case 1:
+				{
+					p1.Czyszczenie_klockow(x1, y1, r_klocka, rotacja);
+					if (p1.czy_ruch_jest_mozliwy(x1 + 1, y1, r_klocka, rotacja))
+					{
+						p1.Generator_Klockow(x1 + 1, y1, r_klocka, rotacja);
+						x1++;
+					}
+					else
+					{
+						p1.Generator_Klockow(x1, y1, r_klocka, rotacja);
+						p1.Czy_usuwac_linie();
+						if (p1.KoniecGry() == true)
+						{
+							cout << "GAME OVER";
+							exit(0);
+						}
+						r_klocka = p1.GetRand();
+						x1 = p1.Pozycja_poczatkowa_x(r_klocka, rotacja);
+						y1 = p1.Pozycja_poczatkowa_y(r_klocka, rotacja);
+						p1.Generator_Klockow(x1, y1, r_klocka, rotacja);
+					}
+
+				}
+				break;
+				case 2:
+				{
+					p1.Czyszczenie_klockow(x1, y1, r_klocka, rotacja);
+					if (p1.czy_ruch_jest_mozliwy(x1, y1 + 1, r_klocka, rotacja))
+					{
+						p1.Generator_Klockow(x1, y1 + 1, r_klocka, rotacja);
+						y1++;
+					}
+					else
+					{
+						p1.Generator_Klockow(x1, y1, r_klocka, rotacja);
+					}
+				}
+				break;
+
+				case 3:
+				{
+					p1.Czyszczenie_klockow(x1, y1, r_klocka, rotacja);
+					if (p1.czy_ruch_jest_mozliwy(x1, y1 - 1, r_klocka, rotacja))
+					{
+						p1.Generator_Klockow(x1, y1 - 1, r_klocka, rotacja);
+						y1--;
+					}
+					else
+					{
+						p1.Generator_Klockow(x1, y1, r_klocka, rotacja);
+					}
+				}
+				break;
+				case 4:
+				{
+					p1.Czyszczenie_klockow(x1, y1, r_klocka, rotacja);
+					rotacja = p1.RotujKlocka1(rotacja);
+					y1 = p1.RotujKlocka2(x1, y1, r_klocka, rotacja);
+					x1 = p1.RotujKlocka3(x1, y1, r_klocka, rotacja);
+				}
+				break;
+				default:
+				{
 					exit(0);
 				}
-				r_klocka = p1.GetRand();
-				x1 = p1.Pozycja_poczatkowa_x(r_klocka, rotacja);
-				y1 = p1.Pozycja_poczatkowa_y(r_klocka, rotacja);
-				p1.Generator_Klockow(x1, y1, r_klocka, rotacja);
-			}
-
-		}
-		break;
-		case 2:
-		{
-			p1.Czyszczenie_klockow(x1, y1, r_klocka, rotacja);
-			if (p1.czy_ruch_jest_mozliwy(x1, y1 + 1, r_klocka, rotacja))
-			{
-				p1.Generator_Klockow(x1, y1 + 1, r_klocka, rotacja);
-				y1++;
-			}
-			else
-			{
-				p1.Generator_Klockow(x1, y1, r_klocka, rotacja);
-			}
-		}
-		break;
-
-		case 3:
-		{
-			p1.Czyszczenie_klockow(x1, y1, r_klocka, rotacja);
-			if (p1.czy_ruch_jest_mozliwy(x1, y1 - 1, r_klocka, rotacja))
-			{
-				p1.Generator_Klockow(x1, y1 - 1, r_klocka, rotacja);
-				y1--;
-			}
-			else
-			{
-				p1.Generator_Klockow(x1, y1, r_klocka, rotacja);
-			}
-		}
-		break;
-		case 4:
-		{
-			p1.Czyszczenie_klockow(x1, y1, r_klocka, rotacja);
-			rotacja = p1.RotujKlocka1(rotacja);
-			y1 = p1.RotujKlocka2(x1, y1, r_klocka, rotacja);
-			x1 = p1.RotujKlocka3(x1, y1, r_klocka, rotacja);
-		}
-		break;
-		default:
-		{
-			exit(0);
-		}
-		}
-		system("cls");
-		p1.Generator_Planszy();
+	
+				}
+				system("cls");
+				p1.Generator_Planszy();
 	}
 	return 0;
 }
